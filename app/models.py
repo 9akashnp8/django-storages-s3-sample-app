e39a0067023beb684b3591390945490a66933488
Akash NP <akashnp1998@gmail.com>
2022-12-24 15:58:12 +0530

    Add Dynamic Path Handling to the Files Being Uploaded
    Files to be stored on Year/Month/File basis based on current
    Year & Month.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
+# Helpers
+def invoice_storage_path(instance, filename):
+    from datetime import datetime
+    TODAY = datetime.now()
+    YEAR = TODAY.strftime('%Y')
+    MONTH = TODAY.strftime('%B')
+    return f'{YEAR}/{MONTH}/{instance.invoice_number}-{filename}'
+
 # Create your models here.
 class Invoice(models.Model):
     invoice_number = models.CharField(max_length=255)
-    invoice_doc = models.FileField()
+    invoice_doc = models.FileField(upload_to=invoice_storage_path)
